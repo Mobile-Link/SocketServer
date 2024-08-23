f54654579068c4b52a0388bcf8753e26f31ebc86
Leonardo <leonardomainardes168@gmail.com>
2024-08-22 21:14:36 -0300

    First commit

---
+++ SocketServer/Program.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+using SocketServer.ChatHub;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddSignalR();
+
+var app = builder.Build();
+
+app.MapGet("/", () => "Hello World!");
+
+app.MapHub<ChatHub>("/chatHub");
+
+app.Run();
