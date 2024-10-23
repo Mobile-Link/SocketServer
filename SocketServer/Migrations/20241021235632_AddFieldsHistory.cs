cdb1d3475b58ad7713a07e2c8503665de03ec6b0
Leonardo <leonardomainardes168@gmail.com>
2024-10-23 20:21:39 -0300

    Add migrations

---
+++ SocketServer/Migrations/20241021235632_AddFieldsHistory.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SocketServer.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddFieldsHistory : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Action",
+                table: "Histories",
+                newName: "IdAction");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "IdAction",
+                table: "Histories",
+                newName: "Action");
+        }
+    }
+}
